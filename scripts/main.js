863c232696893b342ee6378a8b84ef91a9188c68
Adriano Del Pino <adriano.delpino@gmail.com>
2021-10-04 18:52:45 -0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -58,7 +58,7 @@ video.addEventListener('play', async () => {
         canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height);
 
         //faceapi.draw.drawDetections(canvas, resizeDetections);
-        //faceapi.draw.drawFaceLandmarks(canvas, resizeDetections);
+        faceapi.draw.drawFaceLandmarks(canvas, resizeDetections);
         //faceapi.draw.drawFaceExpressions(canvas, resizeDetections);
 
         const maxDescriptorDistance = 0.6;
